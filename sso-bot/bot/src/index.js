05498c0dd07414a14a278937300bb21106f7f9fe
implodingduck <zsek320@gmail.com>
2025-03-28 10:02:27 -0500

    fixing typo GetPhotoAsync to getPhotoAsync

--- sso-bot/bot/src/index.js
+++ sso-bot/bot/src/index.js
@@ -85,6 +85,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 // Listen for incoming requests.
 server.post('/api/messages', async (req, res) => {
     // Route received a request to adapter for processing
-    console.log(`Message: ${req}`);
+    console.log(`Req: ${req}`);
+    console.log(`Req.params: ${req.params}`);
+    console.log(`Req.body: ${req.body}`);
     await adapter.process(req, res, (context) => bot.run(context));
 });
