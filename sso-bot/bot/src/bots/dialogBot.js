ebed2412a522e4265dc70f10bebe76962e0cdf01
implodingduck <zsek320@gmail.com>
2025-04-10 13:47:37 -0500

    adding in AttachmentLayoutTypes

--- sso-bot/bot/src/bots/dialogBot.js
+++ sso-bot/bot/src/bots/dialogBot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { TeamsActivityHandler, CardFactory } = require('botbuilder');
+const { TeamsActivityHandler, CardFactory, AttachmentLayoutTypes } = require('botbuilder');
 
 
 const chartpayload1 = {
