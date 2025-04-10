460efb494cb170fa042122247810d666f43c2f61
implodingduck <zsek320@gmail.com>
2025-04-10 13:32:27 -0500

    adding in some randomization

--- sso-bot/bot/src/bots/dialogBot.js
+++ sso-bot/bot/src/bots/dialogBot.js
@@ -4,61 +4,41 @@
 const { TeamsActivityHandler, CardFactory } = require('botbuilder');
 
 
-const chartpayload = {
+const chartpayload1 = {
     "type": "AdaptiveCard",
     "version": "1.5",
     "body": [
-      {
-        "type": "TextBlock",
-        "text": "Basic",
-        "size": "extraLarge"
-      },
-      {
-        "type": "Chart.Gauge",
-        "value": 50,
-        "segments": [
-          {
-            "legend": "Low risk",
-            "size": 33,
-            "color": "good"
-          },
-          {
-            "legend": "Medium risk",
-            "size": 34,
-            "color": "warning"
-          },
-          {
-            "legend": "High risk",
-            "size": 33,
-            "color": "attention"
-          }
-        ]
-      },
-      {
-        "type": "TextBlock",
-        "text": "Single value",
-        "size": "extraLarge",
-        "spacing": "large",
-        "separator": true
-      },
-      {
-        "type": "Chart.Gauge",
-        "value": 35,
-        "valueFormat": "fraction",
-        "segments": [
-          {
-            "legend": "Used",
-            "size": 35
-          },
-          {
-            "legend": "Unused",
-            "size": 65,
-            "color": "neutral"
-          }
-        ]
-      }
+        {
+            "type": "TextBlock",
+            "text": "Basic",
+            "size": "extraLarge"
+        },
+        {
+            "type": "Chart.Gauge",
+            "value": 50,
+            "segments": [
+                {
+                    "legend": "Low risk",
+                    "size": 33,
+                    "color": "good"
+                },
+                {
+                    "legend": "Medium risk",
+                    "size": 34,
+                    "color": "warning"
+                },
+                {
+                    "legend": "High risk",
+                    "size": 33,
+                    "color": "attention"
+                }
+            ]
+        }
     ]
-  }
+}
+
+
+
 
 /**
  * DialogBot class extends TeamsActivityHandler to handle Teams activities.
@@ -87,8 +67,51 @@ class DialogBot extends TeamsActivityHandler {
         this.userState = userState;
         this.dialog = dialog;
         this.dialogState = this.conversationState.createProperty('DialogState');
+        this.randomnumber = Math.floor(Math.random() * 100);
 
         this.onMessage(this.handleMessage.bind(this));
+    }
+
+    randompayload() {
+        const payload = {
+                "type": "AdaptiveCard",
+                "version": "1.5",
+                "body": [
+                    {
+                        "type": "TextBlock",
+                        "text": "Single value",
+                        "size": "extraLarge",
+                        "spacing": "large",
+                        "separator": true
+                    },
+                    {
+                        "type": "Chart.Gauge",
+                        "value": this.randomnumber,
+                        "valueFormat": "fraction",
+                        "segments": [
+                            {
+                                "legend": "Used",
+                                "size": this.randomnumber
+                            },
+                            {
+                                "legend": "Unused",
+                                "size": (100-this.randomnumber),
+                                "color": "neutral"
+                            }
+                        ]
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": "Action.Execute",
+                        "title": "Randomize",
+                        "data": {
+                            "currentvalue": this.randomnumber
+                        }
+                    }
+                ]
+        };
+        return payload;
     }
 
     /**
@@ -103,15 +126,21 @@ class DialogBot extends TeamsActivityHandler {
         console.log(`Message: ${context.activity.text}`)
         if (context.activity.text === 'login') {
             await this.dialog.run(context, this.dialogState);
-        }else if (context.activity.text === 'card') {
-            await context.sendActivity({ attachments: [
-                CardFactory.adaptiveCard(chartpayload)
-            ]});
+        } else if (context.activity.text === 'card') {
+            await context.sendActivity({
+                attachments: [
+                    CardFactory.adaptiveCard(chartpayload1),
+                    CardFactory.adaptiveCard(this.randompayload()),
+                ],
+                attachmentLayout: AttachmentLayoutTypes.Carousel
+            });
+        } else if (context.activity.text === 'random') {
+            await context.sendActivity(`This current random number is: ${this.randomnumber}`);
         }
         else {
             await context.sendActivity(`Please type "login" to start the authentication process... echoing back your message: ${context.activity.text}`);
         }
-        
+
 
         await next();
     }
